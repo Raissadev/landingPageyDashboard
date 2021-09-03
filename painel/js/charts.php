06044a9d426764488a08396ae461b930555fc252
Raissadev <82960240+Raissadev@users.noreply.github.com>
2021-09-03 15:51:52 -0300

    Update in Charts

--- painel/js/charts.php
+++ painel/js/charts.php
@@ -1,67 +1,56 @@
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script>
-var ctx = document.getElementById('myChart').getContext("2d");
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["JAN", "FEB"],
-        datasets: [{
-            label: "Total",
-            borderColor: "#0a58ca",
-            pointBorderColor: "#0a58ca",
-            pointBackgroundColor: "#0a58ca",
-            pointHoverBackgroundColor: "#0a58ca",
-            pointHoverBorderColor: "#0a58ca",
-            pointBorderWidth: 5,
-            pointHoverRadius: 5,
-            pointHoverBorderWidth: 1,
-            pointRadius: 3,
-            fill: false,
-            borderWidth: 1,
-            data: [0, <?php echo $pegarVisitasTotais; ?>]
-        }]
-    },
-    options: {
-        scales: {
-        yAxes: [{
-            gridLines: {
-                display:false
-            }
-        }], 
-    }
-}
-});
-</script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {
+        'packages':['geochart'],
+      });
+      google.charts.setOnLoadCallback(drawRegionsMap);
 
+      function drawRegionsMap() {
+        var data = google.visualization.arrayToDataTable([
+          ['Country', 'Popularity'],
+          ['Germany', 200],
+          ['United States', 300],
+          ['Brazil', 400],
+          ['Canada', 500],
+          ['France', 600],
+          ['RU', 700]
+        ]);
 
-<script>
-var ctx = document.getElementById('chartRadial').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: [''],
-        datasets: [{
-            label: 'Total de Usuários Online',
-            data: [12, 50],
-            backgroundColor: [
-                '#6a62cc',
-                '#0a58ca'
-            ],
-            borderColor: [
-                'transparent'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-        xAxes: [{
-            gridLines: {
-                display:false
-            }
-        }]
-    }
-}
-});
+        var options = {
+			backgroundColor: 'transparent',
+			datalessRegionColor: '#22223D',
+			defaultColor: '#5a51c7',
+			colorAxis: {colors: ['#5a51c7', '#5a51c7']}
+		};
 
-</script>
+        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+        chart.draw(data, options);
+      }
+    </script>
+	<script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Effort', 'Amount given'],
+          ['My all',     100],
+        ]);
+
+        var options = {
+          pieHole: 0.5,
+		  pieSliceBorderColor: 'transparent',
+		  slices: [{color: '5a51c7'}],
+		  pieSliceTextStyle: {
+            color: 'var(--textColor)',
+          },
+          legend: 'none',
+		  backgroundColor: 'transparent',
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donut_single'));
+        chart.draw(data, options);
+      }
+    </script>
